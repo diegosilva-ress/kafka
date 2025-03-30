135663c7a9c458e04f05544ab098bd87f34a4ebc
diegosilva-ress <8083496+diegosilva-ress@users.noreply.github.com>
2025-03-30 15:21:15 -0300

    Add callback function

--- str-producer/src/main/java/br/com/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/br/com/strproducer/services/StringProducerService.java
@@ -1,17 +1,27 @@
 package br.com.strproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class StringProducerService {
 
   private final KafkaTemplate<String, String> kafkaTemplate;
 
   public void send(String message) {
-    kafkaTemplate.send("str-topic", message);
+    kafkaTemplate.send("str-topic", message).whenComplete((record, ex) -> {
+      if (ex == null) {
+        log.info("Send message with success: {}", message);
+        log.info("Partition {}", record.getRecordMetadata().partition());
+        log.info("Offset {}", record.getRecordMetadata().offset());
+      } else {
+        log.error("Send message with error: {}", ex.getMessage());
+      }
+    });
   }
 
 }
